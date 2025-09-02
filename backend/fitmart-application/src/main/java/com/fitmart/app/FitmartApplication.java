aa863d1c6fa3eba429c2e4e45faafed9891be0a1
upit <upit.upit@enigma.com>
2025-09-02 13:23:55 +0700

    [new project] - FitMart Application

---
+++ backend/fitmart-application/src/main/java/com/fitmart/app/FitmartApplication.java
@@ -0,0 +1,14 @@
+package com.fitmart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class FitmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmartApplication.class, args);
+	}
+
+}
